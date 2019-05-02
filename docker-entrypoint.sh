36fcc41582ea2f69bf94fd4cef98fab90f985817
Franco Rapetti <frapeti@gmail.com>
2019-05-02 16:31:34 +0000

    update to 0.3.1

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,9 +7,9 @@ fi
 
 
 if [ "$PHOTON_MODE" != "CREATE" ] ; then
-	java $JAVA_OPTS -jar photon-0.3.0.jar -nominatim-import -host $NOMINATIM_ADDR -port $NOMINATIM_PORT $PHOTON_OPTS
+	java $JAVA_OPTS -jar photon-0.3.1.jar -nominatim-import -host $NOMINATIM_ADDR -port $NOMINATIM_PORT $PHOTON_OPTS
 else
-	java $JAVA_OPTS -jar photon-0.3.0.jar $PHOTON_OPTS
+	java $JAVA_OPTS -jar photon-0.3.1.jar $PHOTON_OPTS
 fi
 
 
