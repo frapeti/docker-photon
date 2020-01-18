db67b4f2aac1c20167041a78956217a8bd3ad2bb
Franco Rapetti <frapeti@gmail.com>
2020-01-18 03:20:32 -0300

    fix import

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -9,6 +9,13 @@ fi
 if [ "$PHOTON_MODE" != "CREATE" ] ; then
 		java $JAVA_OPTS -jar photon-0.3.2.jar $PHOTON_OPTS
 else
+  printenv
+
+  echo 'java $JAVA_OPTS -jar photon-0.3.2.jar \
+	-nominatim-import -host $PHOTON_ADDR \
+	-user nominatim -password password1234 \
+	-port $PHOTON_PORT -languages es,en $PHOTON_OPTS'
+
 	java $JAVA_OPTS -jar photon-0.3.2.jar \
 	-nominatim-import -host $PHOTON_ADDR \
 	-user nominatim -password password1234 \
